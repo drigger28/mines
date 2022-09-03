1866bcdacc37435837908d034fa21197da4e065f
drigger28 <110015291+drigger28@users.noreply.github.com>
2022-09-03 16:03:18 +0530

    Add files via upload

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,26 @@
-import React from "react";
-import "../App.css"
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 
-const Navbar = () => {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
-                <div className="container">
-                    <a className="navbar-brand fw-bold fs-4" href="Home">
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                        <ul className="dropdown-menu">
-                        </ul>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-                            <li><a className="dropdown-item" href="Home">Home</a></li>
-                            <li><a className="dropdown-item" href="Product">Product</a></li>
-                            <li><a className="dropdown-item" href="Contact">Contact Us</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    );
+const NavBar = () => {
+  return (
+    <>
+      <div class="navbar">
+        <a href="home">Home</a>
+        <a href="Product">Product</a>
+        <a href="Contact">Contact Us</a>
+          
+         
+
+
+          </div>
+       
+     
+    </>
+  );
+
 }
 
-export default Navbar;
+
+
+
+export default NavBar;
